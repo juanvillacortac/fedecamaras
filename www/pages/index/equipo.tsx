7c24e523f1a9766f6af2a244d9f7153f57a02588
Juan Villacorta <juanvillacortac@gmail.com>
2021-03-15 04:05:35 -0400

    Sidebar fix

--- www/pages/index/equipo.tsx
+++ www/pages/index/equipo.tsx
@@ -17,7 +17,7 @@ const Equipo = ({ equipo }: EquipoProps) => (
       </h2>
       <div className="hidden px-8 bg-black lg:block w-4/10 animate" style={{height: '1px !important', ...setAnim({x: '50%'})}}></div>
     </V>
-    <div className="flex flex-wrap -my-8 lg:my-0 mt-16">
+    <div className="flex flex-wrap pt-16 -my-8 lg:my-0">
       {equipo.map((e, i) => (
         <V className="relative w-full py-8 lg:py-0 lg:w-1/4 animate" key={i} style={setAnim({y: '1rem'})}>
           <div className="absolute z-20 mt-4 ml-4 text-xl text-white font-title lg:text-base">

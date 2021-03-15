c7446c66f5cb8094da4365889181b80df9979aa1
Juan Villacorta <juanvillacortac@gmail.com>
2021-03-15 02:32:01 -0400

    Equipo

--- www/pages/index/equipo.tsx
+++ www/pages/index/equipo.tsx
@@ -8,7 +8,7 @@ export interface EquipoProps {
 }
 
 const Equipo = ({ equipo }: EquipoProps) => (
-  <div className="py-16 overflow-hidden content">
+  <div className="py-16 overflow-hidden content" id="equipo">
     <V className="flex flex-row items-center -mx-8 text-kmb-blue-800" oneWay>
       <h2
         className="px-8 text-5xl font-light uppercase font-title animate w-7/10"

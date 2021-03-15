8e21741531d5dcb2654cff8c2ba753fdfe953b22
Juan Villacorta <juanvillacortac@gmail.com>
2021-03-15 02:44:32 -0400

    Equipo

--- www/pages/index/index.tsx
+++ www/pages/index/index.tsx
@@ -4,7 +4,7 @@ import Intro, { IntroProps } from './intro'
 import Equipo, { EquipoProps } from './equipo'
 import Camaras, { CamarasProps } from './camaras'
 
-interface IndexProps extends PageProps, HeroProps, IntroProps, CamarasProps {
+interface IndexProps extends PageProps, HeroProps, IntroProps, EquipoProps, CamarasProps {
 }
 
 const Index = (data: IndexProps) => (

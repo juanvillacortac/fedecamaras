c930b3fbe08b6d6f9f932473f9b0869e0b5d09a0
Juan Villacorta <juanvillacortac@gmail.com>
2021-03-15 00:18:39 -0400

    Camaras

--- components/criticalStyleHead/inlineStyle.tsx
+++ components/criticalStyleHead/inlineStyle.tsx
@@ -15,5 +15,5 @@ export const InlineStyle: React.FC<Props> = ({ assetPrefix, file, nonce }) => {
   const cssSource = readFileSync(cssPath, 'utf-8')
   const html = { __html: cssSource }
   const id = `${assetPrefix}/_next/${file}`
-  return <style dangerouslySetInnerHTML={html} data-href={id} nonce={nonce} />
+  return <style dangerouslySetInnerHTML={html} data-href={id} nonce={nonce}/>
 }

79c1fbbf04d1a37fa089bfc579c122f0beacd569
Juan Villacorta <juanvillacortac@gmail.com>
2021-03-22 15:52:00 -0400

    Nuevo cambio

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,12 +4,6 @@ import Link from 'next/link'
 
 const Page404 = (globalData: any) => (
   <Page title="404" globalData={globalData}>
-    <div className="flex flex-col w-full py-32 text-center text-blue-800 font-title">
-      <h1 className="font-bold text-8xl">404</h1>
-      <Link href="/">
-        <a className="mt-4 t-p hover:underline">Ir al inicio</a>
-      </Link>
-    </div>
   </Page>
 )
 
